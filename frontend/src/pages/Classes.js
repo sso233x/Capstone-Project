201f9c0bcdefc7d07d01c82835e6234f8406162b
sso233x <cogdillmerritt@gmail.com>
2025-04-29 21:45:07 -0400

    Update Classes.js

--- frontend/src/pages/Classes.js
+++ frontend/src/pages/Classes.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./Classes.css";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://capstone-backend-d31e.onrender.com";
 
 function Classes() {
     const [classes, setClasses] = useState([]);
